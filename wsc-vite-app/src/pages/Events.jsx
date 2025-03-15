a5d1bf4be2f34f202df81910e78385a414c1aed5
addetude <addelue@gmail.com>
2025-03-14 22:59:35 -0400

    Updates to Footer

--- wsc-vite-app/src/pages/Events.jsx
+++ wsc-vite-app/src/pages/Events.jsx
@@ -6,6 +6,22 @@ function Events() {
     return (
         <>
             <Nav />
+            <div>
+                <h1 className="">Events</h1>
+            </div>
+            <hr clasNames="border-t-2 border-gray-300 my-4" />
+            <div>
+                <p>yap yap yap</p>
+            </div>
+            <div className="grid grid-cols-1">
+                <hr clasNames="border-t-2 border-gray-300 my-4" />
+                <article>
+                    <h2>Article Title</h2>
+                    <p>Article content...</p>
+                </article>
+                <hr clasNames="border-t-2 border-gray-300 my-4" />
+            </div>
+
             <Footer />
         </>
     )
