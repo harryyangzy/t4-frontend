22e941d4a3d92c78a0c12f19e6b2b4e37c162bfe
Harry! <harryyangzy@gmail.com>
2025-03-18 14:50:06 -0400

    initial commit to about page

--- wsc-vite-app/src/pages/About.jsx
+++ wsc-vite-app/src/pages/About.jsx
@@ -6,6 +6,14 @@ function About() {
     return (
         <>
             <Nav />
+            <div>
+                <h1 className="">About Us</h1>
+            </div>
+            <div className = "justify-center w-[800px]">
+            <hr className="border-t-2 border-gold my-10" />
+                <h3 class = "aboutTitle">Mission</h3>
+                <p>Our mission is to promote the sport of weightlifting in the Washington State area.</p>
+            </div>
             <Footer />
         </>
     )
